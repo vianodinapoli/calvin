eeb88acd66c0a51e696e100a0b039dce00787d6e
Luis Sozinho <sozinholuis@equipmoz.org>
2022-08-08 11:53:08 +0200

    login com senha

--- MeuApp/src/app/login/login.page.ts
+++ MeuApp/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  email: string='';
+  password: string='';
+
+  login(){
+
+    if(this.email == 'adilsonsup@gmail.com' && this.password == '123'){
+      console.log(this.email);
+      console.log(this.password);
+      // Go to home page
+      this.router.navigate(['/home'])
+
+
+
+    }
+    else{
+      console.log('Login Failed');
+    }
+  }
 
   ngOnInit() {
   }
