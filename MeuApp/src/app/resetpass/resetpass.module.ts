a2c7c99bd78e30bfa2d440049509e52f26a43346
Luis Sozinho <sozinholuis@equipmoz.org>
2022-08-19 15:26:36 +0200

    resetPASS

--- MeuApp/src/app/resetpass/resetpass.module.ts
+++ MeuApp/src/app/resetpass/resetpass.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { ResetpassPage } from './resetpass.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ResetpassPageRoutingModule
+    ResetpassPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ResetpassPage]
 })
